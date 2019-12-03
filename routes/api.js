ec15c7115498b9dc89c4a30db7e56ceaa34cf787
Cohen <dcoco91@gmail.com>
2019-12-03 14:42:00 -0500

    working on getting the front end up. Nothing works right now but the layout is laid out

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-
+const url = require("url");
 const validUrl = require("valid-url");
 const shortID = require("shortid");
 module.exports = app => {
@@ -16,40 +16,36 @@ module.exports = app => {
 
   app.post("/api/item", (req, res) => {
     const { ogURL, shortBase } = req.body;
-    console.log(ogURL, shortBase);
-    console.log(validUrl.isUri(shortBase));
-    if (!validUrl.isUri(shortBase)) {
+    // make sure url is kinda valid
+    if (!validUrl.isWebUri(ogURL)) {
       return res.status(401).json("Invalid Base URL");
     }
 
     const URLcode = shortID.generate();
     const createdAt = new Date();
-    if (!validUrl.isHttpUri(ogURL) || !validUrl.isHttpsUri(ogURL)) {
-      db.URL.findOne({ ogURL: ogURL })
-        .then(item => {
-          if (item) {
-            res.status(200).json(item);
-          } else {
-            shortyURL = `${shortBase}/${URLcode}`;
-          }
-          db.URL.create({
-            ogURL: ogURL,
-            code: URLcode,
-            shortyURL: shortyURL,
-            createdAt: createdAt
-          })
-            .then(resp => {
-              res.status(200).json(resp);
-            })
-            .catch(err => {
-              console.log(err);
-            });
+
+    db.URL.findOne({ ogURL: ogURL })
+      .then(item => {
+        if (item) {
+          return res.status(200).json(item);
+        } else {
+          shortyURL = `${shortBase}/${URLcode}`;
+        }
+        db.URL.create({
+          ogURL: ogURL,
+          code: URLcode,
+          shortyURL: shortyURL,
+          createdAt: createdAt
         })
-        .catch(err => {
-          console.log(err, "invalid OG URL");
-        });
-    } else {
-      res.json("error");
-    }
+          .then(resp => {
+            res.status(200).json(resp);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   });
 };
